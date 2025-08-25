3947099d4687233d6670bc3dd5d5bd7dc61f7ba6
Nyi Min htet <6731503078@lamduan.mfu.ac.th>
2025-08-25 09:29:54 +0000

    Done

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -4,16 +4,61 @@ import java.util.Date;
 
 public class Concert {
 
-    //TODO: add attributes
+    private String title;
+    private String description;
+    private Date date;
+    private String location;
+    private double ticketPrice;
 
     public Concert() {
     }
+
     public Concert(String title, String description) {
-        //TODO: set attributes
-
+        this.title = title;
+        this.description = description;
     }
- 
-    //TODO: add getters and setters for all attributes
-    // You can use Source action/generate getter setter function
 
+    public Concert(String title, String description, Date date, String location, double ticketPrice) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.location = location;
+        this.ticketPrice = ticketPrice;
+    }
+
+    // Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getTicketPrice() {
+        return ticketPrice;
+    }
+    public void setTicketPrice(double ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
 }
